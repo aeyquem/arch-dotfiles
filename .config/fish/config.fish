257bc2c567b6775c6dc1e247304067fc539f1fe4
Alejandro <github.overflow216@passmail.net>
2025-08-03 14:10:45 -0400

    added git aliases to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,15 @@ end
 
 zoxide init fish | source
 fzf --fish | source
+
+#Tool aliases
+alias ls eza
+
+# Git Aliases
+alias gcm "git commit -m"
+alias gca "git commit --amend"
+alias gcam "git commit --amend -m"
+
+#Other aliases
+alias cfgsrc "source $HOME/.config/fish/config.fish"
+alias cfgedt "nvim $HOME/.config/fish/config.fish"
